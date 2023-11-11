731233d01d73f5f03bc19801eb46f517748ad425
Ismail Tadji <ismail.tadji@gmail.com>
2023-11-11 18:39:55 +0200

    exercises 1-4

--- src/index.py
+++ src/index.py
@@ -4,16 +4,27 @@ from varasto import Varasto
 def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
+    print_luonti(mehua, olutta)
+    print_olut_getterit(olutta)
+    print_mehu_setterit(mehua)
+    print_virhetilanne()
+    print_liikaa_kaljaa(olutta)
+    print_liian_vahan_mehua(mehua)
+    print_liian_vahan_olutta(olutta)
+    print_liikaa_mehua(mehua)
 
+def print_luonti(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def print_olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def print_mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +33,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def print_virhetilanne():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,22 +43,26 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def print_liikaa_kaljaa(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def print_liian_vahan_mehua(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def print_liian_vahan_olutta(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def print_liikaa_mehua(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
